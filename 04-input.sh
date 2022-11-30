b1e6299cc6b85b5e237745d216b8f14fc85628df
Ayobami O <ayobamiojeniyi@yahoo.com>
2022-11-29 19:43:33 -0600

    MySQL Script

--- 04-input.sh
+++ 04-input.sh
@@ -1,5 +1,10 @@
 read -p 'Enter MySQL Root Username: ' user
 read -s -p 'Enter MySQL Root Password: ' pass
-
+#
 echo "Username & Password from read = $user & $pass"
+
+
+echo Username = $1
+echo Password = $2
+
 
