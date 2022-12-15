20dc5008216c55e30d99ed4d982b88ee6357cfb1
Ayobami O <ayobamiojeniyi@yahoo.com>
2022-12-15 09:14:05 -0600

    MySQL Script

---
+++ 03-if.sh
@@ -0,0 +1,17 @@
+a=1
+
+if [ $a -gt 5 ]
+then
+  echo Yes $a is Greater than 5
+fi
+
+
+name=$1
+case $name in
+  devops)
+    echo Welcome to Devops
+    ;;
+  aws)
+    echo Welcome to AWS
+    ;;
+esac
